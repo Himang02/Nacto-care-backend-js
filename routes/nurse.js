7a00f37fb512d3407c3ff958b3e35c551a9ce77d
Harshil-Jani <harshiljani2002@gmail.com>
2024-03-09 02:38:41 +0530

    Adding response status codes
    Signed-off-by: Harshil-Jani <harshiljani2002@gmail.com>

--- routes/nurse.js
+++ routes/nurse.js
@@ -2,68 +2,78 @@ const getNurses = (req, res, pool) => {
     pool.query('SELECT * FROM nurse', (err, result) => {
         if (err) {
             console.error('Error executing SQL commands', err);
+            res.status(500).json({ error: 'Your request broke our servers :( Working to quickly bring them back to life.' });
         } else {
-            res.json(result.rows);
+            res.status(200).json(result.rows);
         }
     });
 }
 
 
-//name, email, nurse_location, dob, phone_number
-//INSERT INTO nurse (name, email, nurse_location, dob, phone_number) VALUES 
-//-- ('Alice Smith', 'alice@example.com', POINT(40.7128, -74.0060), '1990-05-15', '1234567890'),
+/* name, email, nurse_location, dob, phone_number
+SQL Query:
+
+INSERT INTO nurse (name, email, nurse_location, dob, phone_number) VALUES 
+-- ('Alice Smith', 'alice@example.com', POINT(40.7128, -74.0060), '1990-05-15', '1234567890'),
+
+*/
 const createNurse = (req, res, pool) => {
     console.log(req.body['name']);
     let dateString = req.body['dob'];
     const [year, month, day] = dateString.split("-");
     const sqlDate = new Date(year, month - 1, day);
-    // console.log(sqlDate);
     const formattedSqlDate = sqlDate.toISOString().split('T')[0];
-    // console.log(formattedSqlDate);
     pool.query(`
-    INSERT INTO nurse (name, email, nurse_location, dob, phone_number) VALUES 
-    (
-        '${req.body['name']}',
-        '${req.body['email']}',
-        '${req.body['nurse_location']}',
-        '${formattedSqlDate}',
-        '${req.body['phone_number']}'
-    );
-`, (err, result) => {
+        INSERT INTO nurse (name, email, nurse_location, dob, phone_number) VALUES 
+        (
+            '${req.body['name']}',
+            '${req.body['email']}',
+            '${req.body['nurse_location']}',
+            '${formattedSqlDate}',
+            '${req.body['phone_number']}'
+        );`, (err, result) => {
         if (err) {
             console.error('Error executing SQL commands', err);
+            res.status(500).json({ error: 'Your request broke our servers :( Working to quickly bring them back to life.' });
         } else {
-            res.json(result.rows);
+            res.status(201).json(result.rows);
         }
     });
 
 }
 
 
-// DELETE FROM nurse
-// WHERE id = 'your_nurse_id_here';
+/* id
+SQL Query:
+DELETE FROM nurse WHERE id = 'your_nurse_id_here';
+*/
 const deleteNurse = (req, res, pool) => {
     // console.log(req.body['id']);
     pool.query(`DELETE FROM nurse
         WHERE id = '${req.params['id']}'`, (err, result) => {
         if (err) {
             console.error('Error executing SQL commands', err);
+            res.status(500).json({ error: 'Your request broke our servers :( Working to quickly bring them back to life.' });
         } else {
-            res.json(result.rows);
+            res.status(200).json(result.rows);
         }
     });
 
 }
 
-// UPDATE nurse
-// SET
-//     name = 'new_name',
-//     email = 'new_email',
-//     nurse_location = 'new_location', -- assuming nurse_location is of type POINT
-//     dob = 'new_dob',                 -- assuming dob is of type DATE
-//     phone_number = 'new_phone_number'
-// WHERE id = 'your_nurse_id_here';
+/* name, email, nurse_location, dob, phone_number
 
+SQL Query:
+
+UPDATE nurse 
+SET
+    name = 'new_name',
+    email = 'new_email',
+    nurse_location = POINT(new_latitude, new_longitude),
+    dob = 'new_dob',
+    phone_number = 'new_phone_number'
+WHERE id = 'your_nurse_id_here';
+*/
 const editNurse = async (req, res, pool) => {
     let editKeys = Object.keys(req.body);
     let editKeysSize = editKeys.length;
@@ -84,6 +94,7 @@ const editNurse = async (req, res, pool) => {
     await pool.query(sql, (err, result) => {
         if (err) {
             console.error('Error executing SQL commands', err);
+            res.status(500).json({ error: 'Your request broke our servers :( Working to quickly bring them back to life.' });
         } else {
             console.log('Nurse updated');
         }
@@ -91,11 +102,12 @@ const editNurse = async (req, res, pool) => {
     await pool.query(`\nSELECT * FROM nurse WHERE id='${req.params.id}';`, (err, result) => {
         if (err) {
             console.error('Error executing SQL commands', err);
+            res.status(500).json({ error: 'Your request broke our servers :( Working to quickly bring them back to life.' });
         } else {
             console.log(result.rows);
             const nurseObject = result.rows[0];
             const newNurseObject = Object.assign({}, nurseObject, req.body);
-            res.json(newNurseObject);
+            res.status(200).json(newNurseObject);
         }
     });
 }
